34a0a0b22e58e6fd431e9868aa3d02d8a9457488
Alexander Gorgadze <alexandergorgadze@gmail.com>
2017-03-23 19:35:46 +0400

    test

---
+++ config.rb
@@ -0,0 +1,14 @@
+
+
+
+
+project_type = :stand_alone
+preferred_syntax = :scss
+http_path = '/'
+css_dir = 'css'
+sass_dir = 'css'
+images_dir = 'img'
+javascripts_dir = 'js'
+relative_assets = true
+line_comments = true
+output_style = :expanded
